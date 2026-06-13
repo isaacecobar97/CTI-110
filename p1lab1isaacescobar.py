59ad636a5b50a9210906d7fcda8b781a79c93ea1
isaacecobar97 <escobari6644@student.faytechcc.edu>
2026-06-12 21:34:00 -0400

    First python assigment
    taking input from user and printing output

---
+++ p1lab1isaacescobar.py
@@ -0,0 +1,8 @@
+# Isaac Escobar
+# 12 June 2026
+# p1lab1isaacescobar.py
+# Input and print 
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+print("hello,",first_name,last_name +"!","Welcome to CTI-110!")
